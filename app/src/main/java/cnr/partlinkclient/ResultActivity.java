32f60bd2797eb24bd49fa374a7c8fd826c42a510
lightblue <lightblue502@gmail.com>
2016-02-06 22:41:42 +0700

    [ADD] result have button

--- app/src/main/java/cnr/partlinkclient/ResultActivity.java
+++ app/src/main/java/cnr/partlinkclient/ResultActivity.java
@@ -2,6 +2,8 @@ package cnr.partlinkclient;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends GameActivity {
     private Intent intent;
@@ -16,9 +18,21 @@ public class ResultActivity extends GameActivity {
         intent = getIntent();
         event = intent.getStringExtra("result");
         onGameEvent(event, null);
+
+        ((Button)findViewById(R.id.nextGame)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextGame();
+            }
+        });
     }
     public void ready() {
         gcs.sendGameEvent("result_ready", new String[]{"Ready"});
+    }
+
+    public void nextGame() {
+        ((Button)findViewById(R.id.nextGame)).setEnabled(false);
+        gcs.sendGameEvent("playerConfirm", new String[]{});
     }
 
     public void onGameEvent(String event, String[] params) {

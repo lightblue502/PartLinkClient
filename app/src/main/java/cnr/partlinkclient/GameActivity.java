f79b04985daa1ff21bee81df43eed7f4a6a20dcd
Lucksika Losuvalna <joiiremy@gmail.com>
2016-02-21 20:05:44 +0700

    fix pause

--- app/src/main/java/cnr/partlinkclient/GameActivity.java
+++ app/src/main/java/cnr/partlinkclient/GameActivity.java
@@ -108,9 +108,9 @@ public abstract class GameActivity extends Activity {
             }
         }else if(event.equals("game_resume")){
             isBackPress = false;
+            fragment = null;
             onSuicidePauseFragment();
             ready();
-            fragment = null;
         }
     }
     public abstract void ready();

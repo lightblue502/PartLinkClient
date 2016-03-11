42b23fb6cb15ef48bbbd4ff4a91e443203592474
lightblue <lightblue502@gmail.com>
2016-03-12 03:21:24 +0700

    [UPDATE] QA lanscape and game ready

--- app/src/main/java/cnr/partlinkclient/QAActivity.java
+++ app/src/main/java/cnr/partlinkclient/QAActivity.java
@@ -1,12 +1,11 @@
 package cnr.partlinkclient;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Vibrator;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,7 +14,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.awt.font.TextAttribute;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -25,10 +23,14 @@ public class QAActivity extends GameActivity {
     private Intent intent;
     private String event;
     private boolean isAsk = false;
+    private AnswerFragment answerFragment;
+    private AskFragment askFragment;
+    private Typeface superspaceType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qa);
+        superspaceType = Typeface.createFromAsset(getAssets(), "fonts/Superspace.otf");
         initialServiceBinding();
 
         if (savedInstanceState == null) {
@@ -45,18 +47,21 @@ public class QAActivity extends GameActivity {
     }
 
     public void changeToAnswerFragment(String[] params){
-        AnswerFragment fragment = new AnswerFragment();
-        fragment.setGameCommunicationService(gcs);
-        fragment.setParams(params);
-        replaceFragment(fragment);
+        answerFragment = new AnswerFragment();
+        answerFragment.setGameCommunicationService(gcs);
+        answerFragment.setParams(params);
+        answerFragment.setActivity(this);
+        replaceFragment(answerFragment);
 
 
     }
 
     public void changeToAskFragment(String[] params){
-        AskFragment fragment = new AskFragment();
-        fragment.setParams(params);
-        replaceFragment(fragment);
+        askFragment = new AskFragment();
+        askFragment.setActivity(this);
+        askFragment.setParams(params);
+        askFragment.setGameCommunicationService(gcs);
+        replaceFragment(askFragment);
     }
 
     public void replaceFragment(Fragment fragment){
@@ -92,6 +97,9 @@ public class QAActivity extends GameActivity {
             Log.d("DEBUG_QAactivity","user has reconnect and ready");
             isAsk = false;
             ready();
+        }else if(event.equals("choiceOpen")){
+            answerFragment.setVisibilityButtons(View.VISIBLE);
+            answerFragment.setVisiblityTextView(View.INVISIBLE);
         }
     }
 
@@ -103,7 +111,9 @@ public class QAActivity extends GameActivity {
 
     public static class AnswerFragment extends Fragment {
         public GameCommunicationService gcs;
+        public QAActivity qaActivity;
         public List<Button> btns;
+        public TextView tv;
         public String[] params;
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -122,6 +132,9 @@ public class QAActivity extends GameActivity {
         public void onViewCreated(View view, Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
             generateButton();
+            tv = (TextView) view.findViewById(R.id.listenTv);
+            tv.setTypeface(qaActivity.getTypeFace());
+            setVisiblityTextView(View.VISIBLE);
         }
 
         @Override
@@ -157,9 +170,20 @@ public class QAActivity extends GameActivity {
                         gcs.sendGameEvent("qa_ans", new String[]{text});
                     }
                 });
+                btn.setVisibility(View.INVISIBLE);
+                btn.setTypeface(qaActivity.getTypeFace());
             }
         }
 
+        private void setVisibilityButtons(int value){
+            for (Button btn : btns) {
+                btn.setVisibility(value);
+            }
+        }
+
+        private void setVisiblityTextView(int value){
+            tv.setVisibility(value);
+        }
         private void randomSetValueButton(List<Button> btns, String[] params){
             List<Button> tempBtns = new ArrayList<>();
             tempBtns.addAll(btns);
@@ -171,14 +195,26 @@ public class QAActivity extends GameActivity {
         }
 
 
+        public void setActivity(QAActivity activity) {
+            this.qaActivity = activity;
+        }
+    }
+
+    private Typeface getTypeFace() {
+        return superspaceType;
     }
 
     public static class AskFragment extends Fragment {
         public String[] params;
+        public GameCommunicationService gcs;
         public TextView tv;
+        public TextView questionIcon;
+        private QAActivity qaActivity;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
         }
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -186,12 +222,30 @@ public class QAActivity extends GameActivity {
             // Inflate the layout for this fragment
             return inflater.inflate(R.layout.asker, container, false);
 
+
         }
 
         @Override
         public void onViewCreated(View view, Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
-            getTextView();
+
+            init(view);
+
+            final Button readyQuestion = (Button)view.findViewById(R.id.readyBtn);
+            readyQuestion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    gcs.sendGameEvent("readyQuestion", new String[]{});
+                    setVisibleView();
+                    readyQuestion.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        }
+        public void setVisibleView(){
+            tv.setVisibility(View.VISIBLE);
+            questionIcon.setVisibility(View.VISIBLE);
+
         }
 
         @Override
@@ -203,13 +257,24 @@ public class QAActivity extends GameActivity {
         public void setParams(String[] params){
             this.params = params;
         }
-        public void getTextView(){
-            tv = (TextView)getView().findViewById(R.id.textView);
+        public void setGameCommunicationService(GameCommunicationService gcs){
+            this.gcs = gcs;
+        }
+        public void init(View v){
+            tv = (TextView)v.findViewById(R.id.questionText);
+            tv.setTypeface(qaActivity.getTypeFace());
+            tv.setVisibility(View.INVISIBLE);
+
+            questionIcon = (TextView)getView().findViewById(R.id.questionIcon);
+            questionIcon.setVisibility(View.INVISIBLE);
         }
         public void setTextView(TextView t, String[] params){
             tv.setText(params[0]);
         }
 
+        public void setActivity(QAActivity qaActivity) {
+            this.qaActivity = qaActivity;
+        }
     }
 
     public static class StandbyFragment extends Fragment {

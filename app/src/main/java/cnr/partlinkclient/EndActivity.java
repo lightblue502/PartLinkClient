20d57a0a9aea157e58049d533ca18e0d1cb1c1d1
lightblue <lightblue502@gmail.com>
2016-03-13 18:48:02 +0700

    [ADD] function restart but notwork

--- app/src/main/java/cnr/partlinkclient/EndActivity.java
+++ app/src/main/java/cnr/partlinkclient/EndActivity.java
@@ -1,7 +1,11 @@
 package cnr.partlinkclient;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +24,7 @@ public class EndActivity extends GameActivity {
         event = intent.getStringExtra("end");
         onGameEvent(event, null);
 
-        ((Button)findViewById(R.id.nextGame)).setOnClickListener(new View.OnClickListener() {
+        ((Button)findViewById(R.id.endGame)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 nextGame();
@@ -34,12 +38,45 @@ public class EndActivity extends GameActivity {
     }
 
     public void nextGame() {
-        ((Button)findViewById(R.id.nextGame)).setEnabled(false);
-        gcs.sendGameEvent("playerConfirm", new String[]{});
+        ((Button)findViewById(R.id.endGame)).setEnabled(false);
+        gcs.sendGameEvent("playerRestartGame", new String[]{});
+
+    }
+
+//    public void restartGame(){
+//        Intent restart = getBaseContext().getPackageManager()
+//                .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+//        restart.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(restart);
+//
+//    }
+
+    public void restartGame(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getBaseContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Following code will restart your application after 2 seconds
+        AlarmManager mgr = (AlarmManager) getBaseContext()
+                .getSystemService(Context.ALARM_SERVICE);
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000,
+                pendingIntent);
+
+        //This will finish your activity manually
+        this.finish();
+
+        //This will stop your application and take out from it.
+        System.exit(2);
     }
 
     public void onGameEvent(String event, String[] params) {
         super.onGameEvent(event, params);
+        if (event.equals("restartGame")) {
+            restartGame();
+            gcs.onDestroy();
+        }
     }
 
     @Override
